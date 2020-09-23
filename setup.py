98917c1fdec3e54aae27f3de7be5aabb0a486044
Stephen Ferrari <stephen.ferrari14@gmail.com>
2020-09-22 23:06:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyomc',
-    version='1.0.0',
+    version='1.0.1',
     author='Stephen Ferrari',
     author_email='stephenferrari14@gmail.com',
     description='',
